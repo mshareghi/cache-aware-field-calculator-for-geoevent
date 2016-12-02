a0e5b177f64d8f34b84acf93ad7f45e2f1967e02
Morakot Pilouk <mpilouk@esri.com>
2016-12-01 16:19:58 -0800

    Upgraded to 10.5
    Upgraded to 10.5

--- cache-aware-fieldcalculator-processor/src/main/java/com/esri/geoevent/processor/cacheawarefieldcalculator/CacheAwareFieldCalculatorDefinition.java
+++ cache-aware-fieldcalculator-processor/src/main/java/com/esri/geoevent/processor/cacheawarefieldcalculator/CacheAwareFieldCalculatorDefinition.java
@@ -60,7 +60,7 @@ public class CacheAwareFieldCalculatorDefinition extends GeoEventProcessorDefini
   @Override
   public String getVersion()
   {
-  	return "10.4.0";
+  	return "10.5.0";
   }
   
   @Override
